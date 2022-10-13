c502f66adf6e1bb4c7871a118f69cda2b85d8176
Literal-Eval <pandeyravidev@gmail.com>
2022-10-13 19:17:09 +0530

    Add login functionality

--- src/components/home_page/LoginPage.tsx
+++ src/components/home_page/LoginPage.tsx
@@ -1,7 +1,39 @@
+import { useContext, useRef, useState } from "react";
+import { Spinner } from "react-bootstrap";
 import backgroundImg from "../../assets/images/parties-events.svg";
+import AppContext from "../../context/app_context";
 import "../../styles/input_style.css";
+import tryFirebaseLogin from "./login_page";
 
 const LoginPage = (): JSX.Element => {
+    const emailRef = useRef<HTMLInputElement>(null);
+    const passRef = useRef<HTMLInputElement>(null);
+    const appCtx = useContext(AppContext);
+
+    let [isLoggingIn, setIsLogginIn] = useState(false);
+    let [invalidCreds, setInvalidCreds] = useState(false);
+
+    const tryLogin = (): void => {
+        setIsLogginIn(true);
+        console.log(emailRef.current?.value, passRef.current?.value);
+
+        tryFirebaseLogin(emailRef.current?.value, passRef.current?.value)
+            .then((result: boolean) => {
+                console.log("Login", appCtx.pageNumber);
+                setInvalidCreds(!result);
+                setIsLogginIn(false);
+            })
+            .catch((e) => {
+                console.log(e);
+                setInvalidCreds(true);
+                setIsLogginIn(false);
+            });
+    };
+
+    const inputOnFocus = (): void => {
+        setInvalidCreds(false);
+    };
+
     return (
         <section
             style={{
@@ -31,18 +63,39 @@ const LoginPage = (): JSX.Element => {
                 <h1 style={{ fontSize: "76px" }}>Admin Login</h1>
                 <div style={{ height: "5vh" }}></div>
                 <input
-                    className="app-input-line"
+                    ref={emailRef}
+                    onFocus={inputOnFocus}
+                    className={`app-input-line ${
+                        invalidCreds ? "invalidCreds" : ""
+                    }`}
                     type="string"
                     inputMode="email"
                     placeholder="Email"
                 />
                 <input
-                    className="app-input-line"
+                    ref={passRef}
+                    onFocus={inputOnFocus}
+                    className={`app-input-line ${
+                        invalidCreds ? "invalidCreds" : ""
+                    }`}
                     type="password"
                     placeholder="Password"
                 />
-                <div style={{ height: "5vh" }}></div>
-                <button className="app-link">Login</button>
+                <div style={{ height: "5vh" }}>
+                    <p
+                        style={{
+                            height: invalidCreds ? "100%" : "0%",
+                            overflow: "clip",
+                        }}
+                    >
+                        Invalid Email/Password
+                    </p>
+                </div>
+                <button onClick={tryLogin} className="app-link app-link--login">
+                    &nbsp;
+                    {isLoggingIn ? <Spinner animation="border" /> : "Login"}
+                    &nbsp;
+                </button>
             </div>
         </section>
     );
---
+++ src/components/home_page/login_page.ts
@@ -0,0 +1,22 @@
+// import { createUserWithEmailAndPassword } from "firebase/auth";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { appAuth } from "../../firebase/firebase";
+
+const tryFirebaseLogin = async (
+    email: string | undefined,
+    password: string | undefined
+): Promise<boolean> => {
+    if (!email || !password) {
+        return false;
+    }
+
+    // await createUserWithEmailAndPassword(appAuth, email!, password!);
+
+    // console.log(
+    await signInWithEmailAndPassword(appAuth, email!, password!);
+    // );
+
+    return true;
+};
+
+export default tryFirebaseLogin;
