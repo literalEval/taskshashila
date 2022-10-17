63c2eef59f3ab574dc0bd3c0de156a5a1f5f6d5a
Kavitajaya <kavitajaya7@gamil.com>
2022-10-17 22:15:17 +0530

    add results page

--- src/pages/home_page/sub_pages/Results.tsx
+++ src/pages/home_page/sub_pages/Results.tsx
@@ -1,5 +1,15 @@
 const Results = (): JSX.Element => {
-    return <section></section>
-}
+    return (
+        <section
+            style={{
+                height: "100vh",
+                padding: "10% 10% 0% 10%",
+
+                backgroundColor: "#f1eee8",
+                zIndex: 2,
+            }}
+        ></section>
+    );
+};
 
 export default Results;
